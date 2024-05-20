65b314ba8011602929710dfe3b590295c38a9ab8
VuongTranDev <vuongtran1726@gmail.com>
2024-05-20 22:42:46 +0700

    push web.php

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,144 @@ Route::get('/allProducts', [
     ProductsController::class,
     "allProducts"
 ]);
+
+Route::get('/products/{masanpham}', [
+    ProductsController::class, 
+    'showDetailProduct'
+])->name('product.showDetailProduct');
+
+Route::get('/products/productsByType/{tenloai}', [
+    ProductsController::class, 
+    'productsByType' 
+])->name('products.productsByType');
+
+Route::post('/danhgias/themDanhGia', [
+    DanhGiaController::class,
+    'themDanhGIa'
+])->name('danhgias.themDanhGia');
+
+Route::get('/danhgias/showAllComment', [
+    DanhGiaController::class,
+    'showAllComment'
+]);
+
+Route::get('/danhgias/filterByRating', [
+    DanhGiaController::class,
+    'filterByRating'
+]);
+
+Route::get('/danhgias/showDanhGia', [
+    DanhGiaController::class, 
+    'showDanhGia'
+]);
+
+Route::get('/admin_login',[
+    AdminController::class,
+    "index"
+]) ;
+Route::get('/admin_content',[
+    AdminController::class,
+    "adminlayout"
+]) ;
+
+
+Route::post('/admin_TK',[
+    AdminController::class,
+    "login"
+]) ;
+
+
+Route::post('/DangKiTK',[
+    AdminController::class,
+    "register"
+]) ;
+Route::get('/logout',[
+    AdminController::class,
+    "logout"
+]) ;
+Route::get('/thongKeDS',[
+    AdminController::class,
+    "thongKeDS"
+]) ;
+Route::post('/thongKeSanLuong',[
+    AdminController::class,
+    "thongKeSanLuong"
+]) ;
+// CategoryProduct
+
+Route::get('/addCategoryProduct', [
+    CategoryProductController::class,
+    "addProduct"
+]);
+
+Route::post('/saveCategoryProduct', [
+    CategoryProductController::class,
+    "saveCategoryProduct"
+]);
+
+Route::get('/editCategoryProduct/{ID}', [
+    CategoryProductController::class,
+    "editCategoryProduct"
+]);
+Route::post('/updateCategoryProduct{ID}', [
+    CategoryProductController::class,
+    "updateCategoryProduct"
+]);
+
+Route::get('/deleteCategoryProduct/{ID}', [
+    CategoryProductController::class,
+    "deleteCategoryProduct"
+]);
+
+//Brand Controller
+
+Route::get('/addbrands', [
+    BrandController::class,
+    "addBrand"
+]);
+Route::post('/saveBrand', [
+    BrandController::class,
+    "SaveBrand"
+]);
+Route::get('/editBrand/{ID}', [
+    BrandController::class,
+    "editBrand"
+]);
+Route::post('/updateBrand{ID}', [
+    BrandController::class,
+    "updateBrand"
+]);
+
+Route::get('/deleteBrand/{ID}', [
+    BrandController::class,
+    "deleteBrand"
+]);
+
+// DetailPRoduc -- ADMIN
+Route::get('/addDetailProduct',
+[
+    DetailsProductController::class,
+    "addDetailProduct"
+]);
+
+Route::post('/saveDetailProduct',
+[
+    DetailsProductController::class,
+    "saveDetailProduct"
+]);
+Route::get('/allDetailProduct',
+[
+    DetailsProductController::class,
+    "allDetailProDuct"
+]);
+
+Route::get('/editDetailProduct/{ID}',
+[
+    DetailsProductController::class,
+    "editDetailProDuct"
+]);
+Route::post('/updateDetailProduct{ID}',
+[
+    DetailsProductController::class,
+    "updateDetailProduct"
+]);
