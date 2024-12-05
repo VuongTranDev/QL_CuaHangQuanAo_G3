42930ddc7b6582f025f7114d5b8469c5ed6977e9
VuongTranDev <vuongtran975@gmail.com>
2024-12-05 21:54:53 +0700

    hidden MACTHD

--- resources/views/danhgias/themDanhGia.blade.php
+++ resources/views/danhgias/themDanhGia.blade.php
@@ -10,11 +10,6 @@
             @endforeach
             <form class="form-comment" role="comment" action="{{ route('danhgias.themDanhGia') }}" method="POST">
                 @csrf
-                @php
-                    $MACTHD = Session::get('macthd');
-                    if(Session::get('macthd'))
-                        echo $MACTHD;
-                @endphp
                 <input type="hidden" name="MACTHD" value="{{$MACTHD}}">
                 <div class="avaliacou" align="center">
                     <label for="star1" class="star-icon" data-avaliacao="1">
